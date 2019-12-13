1c375f15462b596ec49114579f82a4cceafb62d8
Jeremy Leonardo <jeremy.leonardo@atomic.id>
2019-12-13 07:39:40 +0700

    Update contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -19,13 +19,12 @@
           <div class="col-md-12 text-center pb-5 mb-5">
             <h3 class="pb-3">Our Team</h3>
             <p>2201731106 - JEREMY LEONARDO</p>
-            <p>22017 - </p>
-            <p>22017 - </p>
-            <p>22017 - </p>
-            <p>22017 - </p>
+            <p>2201829666 -	Aprishiela Hartono Putri</p>
+            <p>2201734404	- Gabriel Aldi</p>
+            <p>2201768626	- Juan Jonhart Jonathan</p>
+            <p>2201829943	- Reyhan Satriya</p>
           </div>
         </div>
-        
 
       </div>
     </div>
