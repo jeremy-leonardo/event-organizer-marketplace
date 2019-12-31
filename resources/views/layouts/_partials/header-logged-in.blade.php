0bd31523fd2bffa6a580f31ab662c5c56c23cd78
Jeremy Leonardo <jeremyleonardo017@gmail.com>
2020-01-01 00:51:33 +0700

    Display top header logged in for mobile view

--- resources/views/layouts/_partials/header-logged-in.blade.php
+++ resources/views/layouts/_partials/header-logged-in.blade.php
@@ -1,7 +1,7 @@
-<div class="top_menu">
+<div class="top_menu" style="display:block;">
   <div class="container">
     <div class="row">
-      <div class="col-lg-7">
+      <div class="col-7">
         <div class="float-left">
           <!-- <p>Phone: +0123456789</p> -->
           {{-- <p>Support email: jeremy.leonardo@binus.ac.id</p> --}}
@@ -13,7 +13,7 @@
           @endauth
         </div>
       </div>
-      <div class="col-lg-5">
+      <div class="col-5">
         <div class="float-right">
           <ul class="right_side">
             @auth('user')
